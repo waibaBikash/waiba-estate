036b27e54b215804d43b5ae6fe450ae92bb5a694
waibaBikash <kavrelibikash@gmail.com>
2024-12-01 22:41:53 +1100

    create navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,15 @@ import { assets } from '../assets/assets'
 const Navbar = () => {
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
-         <div>
+         <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
             <img src={assets.logo} alt="" />
+             <ul className='hidden md:flex gap-7 text-white'>
+                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Home</a>
+                <a href="#Header" className='cursor-pointer hover:text-gray-400'>About</a>
+                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Projects</a>
+                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
+             </ul>
+             <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
          </div>
     </div>
   )
