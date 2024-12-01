da11328cbae8af515c321e4efa8e22f36e8795b3
waibaBikash <kavrelibikash@gmail.com>
2024-12-01 23:00:27 +1100

    create a header and outfit font

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
+    <div className='w-full overflow-hidden'>
+      <Header/>
     </div>
   )
 }
