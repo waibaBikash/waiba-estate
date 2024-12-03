7ecfda2df82645c8f1b4dc31be4b304167db67ce
waibaBikash <kavrelibikash@gmail.com>
2024-12-03 20:10:15 +1100

    commit change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,9 +20,9 @@ const Navbar = () => {
             <img src={assets.logo} alt="" />
              <ul className='hidden md:flex gap-7 text-white'>
                 <a href="#Header" className='cursor-pointer hover:text-gray-400'>Home</a>
-                <a href="#Header" className='cursor-pointer hover:text-gray-400'>About</a>
-                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Projects</a>
-                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
+                <a href="#About" className='cursor-pointer hover:text-gray-400'>About</a>
+                <a href="#Projects" className='cursor-pointer hover:text-gray-400'>Projects</a>
+                <a href="#Testimonials" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
              </ul>
              <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
               <img onClick={()=> setShowMobleMenu(true)} src={assets.menu_icon} className='md:hidden w-7 cursor-pointer' alt="" />
