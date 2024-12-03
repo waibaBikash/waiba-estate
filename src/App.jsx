694d223af9ecfc920a6d8bc9d167c615bc973a79
waibaBikash <kavrelibikash@gmail.com>
2024-12-03 21:21:57 +1100

    create testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
+import Testimonials from './components/Testimonials'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header/>
       <About/>
       <Projects/>
+      <Testimonials/>
     </div>
   )
 }
