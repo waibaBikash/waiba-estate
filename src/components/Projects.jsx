6e39e4f094d7b68b22327aba0791d332f2044eea
waibaBikash <kavrelibikash@gmail.com>
2024-12-03 21:02:11 +1100

    project slider functionality

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets, projectsData } from '../assets/assets'
 
 const Projects = () => {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [cardsToShow, setCardsToShow] = useState(1);
+
+  useEffect(()=>{
+     const updateCardsToShow = ()=>{
+       if(window.innerWidth >= 1024){
+         setCardsToShow(projectsData.length);
+       }else{
+        setCardsToShow(1)
+       }
+      };
+       updateCardsToShow();
+
+       window.addEventListener('resize', updateCardsToShow);
+       return ()=> window.removeEventListener('resize', updateCardsToShow);
+     
+  },[])
+
+  const nextProject = ()=>{
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projectsData.length)
+  }
+  const prevProject = ()=>{
+    setCurrentIndex((prevIndex) => prevIndex === 0 ? projectsData.length - 1 : prevIndex -1)
+  }
   return (
     <div className='container mx-auto py-4 pt-20 px-6 md:px-20'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
       <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Crafting Spaces, Building Legacies-Explore Our Portfolio</p>
       {/* Slider buttons */}
         <div className='flex justify-end items-center mb-8'>
-           <button className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous Project'>
+           <button onClick={prevProject} className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous Project'>
              <img src={assets.left_arrow} alt="Previous" />
            </button>
-           <button className='p-3 bg-gray-200 rounded mr-2' aria-label='Next Project'>
+           <button onClick={nextProject} className='p-3 bg-gray-200 rounded mr-2' aria-label='Next Project'>
              <img src={assets.right_arrow} alt="Next" />
            </button>
         </div>
         {/* Projects Slider Container */}
          <div className='overflow-hidden'>
-            <div className='flex gap-8 transition-transform duration-500 ease-in-out'>
+            <div className='flex gap-8 transition-transform duration-500 ease-in-out'
+             style={{transform: `translateX(-${(currentIndex * 100) / cardsToShow}%)`}}
+            >
                {projectsData.map((project, index)=>(
                  <div key={index} className='relative flex-shrink-0 w-full sm:w-1/4'>
                    <img src={project.image} alt={project.title} className='w-full h-auto mb-14' />
