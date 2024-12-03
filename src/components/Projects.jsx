fdcd9dd1fdec6f006794a609be719c33c4cac97f
waibaBikash <kavrelibikash@gmail.com>
2024-12-03 21:23:56 +1100

    save change

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -51,7 +51,7 @@ const Projects = () => {
                     <div className='absolute left-0 right-0 bottom-5 flex justify-center'>
                         <div className="inline-block bg-white w-3/4 px-4 py-2 shadow-md">
                            <h2 className='text-xl font-semibold text-gray-800'>{project.title}</h2>
-                           <p className='text-gray-500 text-sm'>{project.price} <span>|</span>{project.location}</p>
+                           <p className='text-gray-500 text-sm'>{project.price} <span className='px-1'>|</span>{project.location}</p>
                         </div>
                     </div>
                  </div>
