34e9a49a56d641ca246a2b50aff590b1800fb158
waibaBikash <kavrelibikash@gmail.com>
2024-12-04 22:50:13 +1100

    add react toastify

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
+      <ToastContainer/>
       <Header/>
       <About/>
       <Projects/>
