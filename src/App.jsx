534eb093c41289f2a4bd4efcf3ba6a502e688394
waibaBikash <kavrelibikash@gmail.com>
2024-12-04 23:14:53 +1100

    complete footer section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Projects/>
       <Testimonials/>
       <Contact/>
+      <Footer/>
     </div>
   )
 }
