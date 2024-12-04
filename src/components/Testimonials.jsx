4be80a85ac09eefe7d91878fbe4435a86f9c07b9
waibaBikash <kavrelibikash@gmail.com>
2024-12-04 22:08:07 +1100

    scroll somoothly

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,7 +3,7 @@ import { assets, testimonialsData } from '../assets/assets'
 
 const Testimonials = () => {
   return (
-    <div className='container mx-auto py-10 lg:px-32 w-full overflow-hidden'>
+    <div className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonials'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Customer <span className='uderline underline-offset-4 decoration-1 under font-light'>Testimonials</span></h1>
       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Real Stores from Those Who Found Home <br /> with Us</p>
        <div className='flex flex-wrap justify-center gap-8'>
