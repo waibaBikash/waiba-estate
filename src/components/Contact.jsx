2419e30fca8fec9724c3d633998013e1bdc536d9
waibaBikash <kavrelibikash@gmail.com>
2024-12-04 22:42:47 +1100

    form submit functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
 
 const Contact = () => {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "bed04237-5dbf-4551-981d-bab8dd3e0f7d");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      alert("Form Submitted Successfully")
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      alert(data.message)
+      setResult();
+    }
+  };
   return (
     <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
        <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact <span className='uderline underline-offset-4 decoration-1 under font-light'>With Us</span></h1>
        <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Ready to Make a Move? Let's Build Your Future Together <br /> with Us</p>
 
 
-       <form className='max-w-2xl  mx-auto text-gray-600 pt-8'>
+       <form onSubmit={onSubmit} className='max-w-2xl  mx-auto text-gray-600 pt-8'>
            <div className='flex flex-wrap'>
               <div className='w-full md:w-1/2 text-left'>
                 Your Name
@@ -22,7 +49,7 @@ const Contact = () => {
                  Message 
                  <textarea className='w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none' name="Message" placeholder='Message' required></textarea>
               </div>
-              <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded'>Send Message</button>
+              <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded'>{result ? result : "Send Message"}</button>
        </form>
     </div>
   )
