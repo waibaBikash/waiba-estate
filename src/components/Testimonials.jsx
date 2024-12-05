f91cb775ce6769a0f5fa4de5da54254e0d46f7c7
waibaBikash <kavrelibikash@gmail.com>
2024-12-05 22:56:07 +1100

    animation in testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { assets, testimonialsData } from '../assets/assets'
+import { motion } from 'framer-motion'
 
 const Testimonials = () => {
   return (
-    <div className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonials'>
+    <motion.div 
+    initial={{opacity: 0, x:200}}
+        transition={{duration: 1}}
+        whileInView={{opacity: 1,  x: 0}}
+        viewport={{once: true}}
+    className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonials'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Customer <span className='uderline underline-offset-4 decoration-1 under font-light'>Testimonials</span></h1>
       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Real Stores from Those Who Found Home <br /> with Us</p>
        <div className='flex flex-wrap justify-center gap-8'>
@@ -21,7 +27,7 @@ const Testimonials = () => {
              </div>
            ))}
        </div>
-    </div>
+    </motion.div>
   )
 }
 
