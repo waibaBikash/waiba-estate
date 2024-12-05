b6f36819abc4c6c41ec450a63a0763680ac7f060
waibaBikash <kavrelibikash@gmail.com>
2024-12-05 22:49:42 +1100

    animation in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { motion } from 'framer-motion'
 
 const About = () => {
   return (
-    <div className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id='About'>
+    <motion.div 
+    initial={{opacity: 0, x:200}}
+    transition={{duration: 1}}
+    whileInView={{opacity: 1,  x: 0}}
+    viewport={{once: true}}
+    className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id='About'>
        <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About <span className='underline underline-offset-4 decoration-1 under font-light'>Our Brand</span></h1>
        <p className='text-gray-500 max-w-80 text-center mb-8'>Passinate About Properties, Dedicated to Your Vision</p>
         <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
@@ -33,7 +39,7 @@ const About = () => {
                   <button className='bg-blue-600 text-white px-8 py2 rounded'>Learn More</button>
             </div>
         </div>
-    </div>
+    </motion.div>
   )
 }
 
