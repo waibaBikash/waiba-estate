f1159c0396fba48ed330f1bd0792b0efcdedafb0
waibaBikash <kavrelibikash@gmail.com>
2024-12-06 22:29:41 +1100

    save change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,11 @@ const About = () => {
     whileInView={{opacity: 1,  x: 0}}
     viewport={{once: true}}
     className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id='About'>
-       <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About <span className='underline underline-offset-4 decoration-1 under font-light'>Our Brand</span></h1>
+       <h1 
+       className='text-2xl sm:text-4xl font-bold mb-2'>
+        About <span className='underline underline-offset-4 decoration-1 under font-light'>
+          Our Brand</span>
+          </h1>
        <p className='text-gray-500 max-w-80 text-center mb-8'>Passinate About Properties, Dedicated to Your Vision</p>
         <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
            <img src={assets.brand_img} alt="" className='w-full sm:w-1/2 maxwlg' />
