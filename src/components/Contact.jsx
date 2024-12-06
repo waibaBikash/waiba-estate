3f245d749439e0a5e650cba8efc29166f4f5e109
waibaBikash <kavrelibikash@gmail.com>
2024-12-06 22:26:14 +1100

    commit change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
         viewport={{once: true}}
     className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
        <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact <span className='uderline underline-offset-4 decoration-1 under font-light'>With Us</span></h1>
-       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Ready to Make a Move? Let's Build Your Future Together <br /> with Us</p>
+       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Ready to Make a Move? Let's Build Your Future Together  with Us</p>
 
 
        <form onSubmit={onSubmit} className='max-w-2xl  mx-auto text-gray-600 pt-8'>
