f1c5b456684d8151f310b729ab10cd9fa80612c6
waibaBikash <kavrelibikash@gmail.com>
2024-12-07 21:52:08 +1100

    changes made

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,8 +35,15 @@ const Projects = () => {
     whileInView={{opacity: 1,  x: 0}}
     viewport={{once: true}}
     className='container mx-auto py-4 pt-20 px-6 md:px-20'>
-      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
-      <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Crafting Spaces, Building Legacies-Explore Our Portfolio</p>
+      <h1 
+      className='text-2xl sm:text-4xl font-bold mb-2 text-center'>
+        Projects <span className='underline underline-offset-4 decoration-1 under font-light'>
+          Completed</span>
+          </h1>
+      <p 
+      className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>
+        Crafting Spaces, Building Legacies-Explore Our Portfolio
+        </p>
       {/* Slider buttons */}
         <div className='flex justify-end items-center mb-8'>
            <button onClick={prevProject} className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous Project'>
@@ -57,7 +64,10 @@ const Projects = () => {
                     <div className='absolute left-0 right-0 bottom-5 flex justify-center'>
                         <div className="inline-block bg-white w-3/4 px-4 py-2 shadow-md">
                            <h2 className='text-xl font-semibold text-gray-800'>{project.title}</h2>
-                           <p className='text-gray-500 text-sm'>{project.price} <span className='px-1'>|</span>{project.location}</p>
+                           <p 
+                           className='text-gray-500 text-sm'>{project.price} 
+                           <span className='px-1'>|</span>{project.location}
+                           </p>
                         </div>
                     </div>
                  </div>
