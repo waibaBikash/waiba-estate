ecbb18e2aa633d477968d3161111d35fe1f36db1
waibaBikash <kavrelibikash@gmail.com>
2024-12-07 21:48:34 +1100

    changes made

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,11 @@ const Footer = () => {
       <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
          <div className='w-full md:w-1/3 mb-8 md:mb-0'>
            <img src={assets.logo_dark} alt="" />
-           <p className='text-gray-400 mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis nobis mollitia officia ipsa ad veritatis veniam? Vero doloremque explicabo id!</p>
+           <p 
+           className='text-gray-400 mt-4'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Quis nobis mollitia officia ipsa ad veritatis veniam? Vero doloremque explicabo id!
+             </p>
          </div>
          <div className='w-full md:w-1/5 mb-8 md: mb-0'>
            <h3 className='text-white text-lg font-bold mb-4'>Company</h3>
@@ -22,7 +26,10 @@ const Footer = () => {
            <h3 className='text-white text-lg font-bold mb-4'>Subscribe to our newsletter</h3>
            <p className='text-gray-400 mb-4 min-w-80'>The latest news, articles, and resources, sent to your inbox weekly. </p>
             <div className='flex gap-2'>
-               <input type="email" placeholder='Enter your email' className='p-2 rounded bg-gray-800 text-gray-400 border border-gray-700 focus:outline-none w-full md:w-auto ' />
+               <input 
+               type="email" placeholder='Enter your email' 
+               className='p-2 rounded bg-gray-800 text-gray-400 border border-gray-700 focus:outline-none w-full md:w-auto ' 
+               />
                <button className='py-2 px-4 rounded bg-blue-500 text-white'>Subscribe</button>
             </div>
          </div>
