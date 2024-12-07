905af723a89c3fa9ba3f03d8c8eab616902b85ab
waibaBikash <kavrelibikash@gmail.com>
2024-12-07 21:49:48 +1100

    save change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Navbar from './Navbar'
-import { motion } from 'framer-motion'
+import React from 'react';
+import Navbar from './Navbar';
+import { motion } from 'framer-motion';
 
 const Header = () => {
   return (
@@ -30,4 +30,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
